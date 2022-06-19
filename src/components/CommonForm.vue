d71a861c653e949b46cbdef346d36a3acbde229a
matheus-paulino <matheuspaulino.santos1@gmail.com>
2022-06-19 02:59:53 -0300

    add new todo

--- src/components/CommonForm.vue
+++ src/components/CommonForm.vue
@@ -1,12 +1,12 @@
 <template>
-  <form class="flex items-center px-4 bg-gray-900 h-15 rounded-sm border-l-2 border-green-400 mb-3">
+  <form @submit.stop.prevent="submit" class="flex items-center px-4 bg-gray-900 h-15 rounded-sm border-l-2 border-green-400 mb-3">
     <input
         placeholder="Adicione um novo item ..."
         type="text"
         class="bg-gray-900 placeholder-gray-500 text-gray-500
 font-light focus:outline-none block w-full appearance-none leading-normal
 py-3 pr-3"
-    >
+     v-model="title">
 
     <button
         class="text-green-400 text-xs font-semibold
@@ -20,7 +20,26 @@ focus:outline-none"
 
 <script>
 export default {
-  name: "CommonForm"
+  name: "CommonForm",
+  data() {
+    return {
+      title: ''
+    }
+  },
+
+  methods: {
+    submit() {
+      if (!this.title) {
+        return;
+      }
+      this.$store.dispatch('addTodo', {
+        title: this.title,
+        completed: false
+      }).finally(() => {
+        this.title = ''
+      })
+    }
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,9 @@ export default createStore({
     mutations: {
         storeTodos(state, payload) {
             state.todos = payload
+        },
+        storeTodo(state, payload) {
+            state.todos.push(payload)
         }
     },
     actions: {
@@ -22,6 +25,13 @@ export default createStore({
                         })
                 }, 2000)
             })
+        },
+
+        addTodo({commit}, payload) {
+            return axios.post('http://localhost:3000/todos', payload)
+                .then((response) => {
+                    commit('storeTodo', response.data)
+                })
         }
     }
 })
